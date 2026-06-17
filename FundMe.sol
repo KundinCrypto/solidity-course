3263bacbf809f1408e2e0899a0a33bb038e80454
Yasin Abdulwahab Musa <kundincrypto@gmail.com>
2026-06-17 01:06:49 +0100

    Adding fallback exmple

--- FundMe.sol
+++ FundMe.sol
@@ -3,22 +3,62 @@ pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+// constant, immutable
+
+// 867,041
+// 846,656
+
+error NotOwner();
 contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public immutable i_owner;
+    // 439
+    // 2574
+
+    constructor () {
+        i_owner = msg.sender;
+    }
+
     function fund() public payable {
         msg.value.getConversionRate();
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw() public {}
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+        revert();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not owner");
+        if (msg.sender != i_owner) {revert NotOwner();}
+        _;
+    }
+
+    // What happen if someone sends ETH to this contract without calling the fund function?
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
    
 }
